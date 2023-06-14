27e65488dae53506af1db1a6289f26910ff4d597
Capitán Nemo <71532630+BrunoE2001@users.noreply.github.com>
2023-06-14 15:46:27 -0600

    Add files via upload

---
+++ GraphQLApollo/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_esaudev_graphqlapollo_GraphQlApp.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "com.esaudev.graphqlapollo.GraphQlApp"
+)
+public final class _com_esaudev_graphqlapollo_GraphQlApp {
+}
---
+++ GraphQLApollo/app/build/generated/source/kapt/debug/com/esaudev/graphqlapollo/GraphQlApp_GeneratedInjector.java
@@ -0,0 +1,15 @@
+package com.esaudev.graphqlapollo;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.components.SingletonComponent;
+import dagger.hilt.internal.GeneratedEntryPoint;
+
+@OriginatingElement(
+    topLevelClass = GraphQlApp.class
+)
+@GeneratedEntryPoint
+@InstallIn(SingletonComponent.class)
+public interface GraphQlApp_GeneratedInjector {
+  void injectGraphQlApp(GraphQlApp graphQlApp);
+}
